82ffaa15b5011039ec6884eb2a116eac49883e1f
fawazbruce <99738657+fawazbruce@users.noreply.github.com>
2023-01-03 02:45:53 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,7 @@ sonarqubeScanner({
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.projectKey':'NodeJsColdEnergyTeck',
 	    'sonar.login': 'ae4e7475f3fbefc000c78201fa36c98c1024b275',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
